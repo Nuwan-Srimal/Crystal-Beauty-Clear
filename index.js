1cd5af79c57c2665b686c0395dc998580fc84fa1
Nuwan Srimal <112359694+Nuwan-Srimal@users.noreply.github.com>
2025-07-15 07:30:23 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,32 @@
+import express from "express";
+import mongoose from "mongoose";
+import studentRouter from "./routes/studentsRouter.js";
+import userRouter from "./routes/userRouter.js";
+
+const app = express()
+
+app.use(express.json())
+
+const connectionString = "mongodb+srv://admin:123@cluster0.qnd4lep.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+
+
+mongoose.connect(connectionString).then(
+    ()=>{
+        console.log("Database connected")
+    }
+).catch(
+    ()=>{
+        console.log("Database connection failed")
+    }
+)
+
+
+app.use("/students",studentRouter)
+app.use("/users", userRouter)
+
+app.listen(5000, 
+    ()=>{
+        console.log("Server is started")
+        console.log("Thank you")
+    }
+)
